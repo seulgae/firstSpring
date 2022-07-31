400ae3333af4fd43c3d92fba627e2a401aae55d8
seulgae <r1605866@naver.com>
2022-08-01 04:04:14 +0900

    ajax 테스트 댓글 게시판 연동준비

---
+++ src/main/java/com/firstSpring/app/controller/SimpleRestController.java
@@ -0,0 +1,33 @@
+package com.firstSpring.app.controller;
+
+import com.firstSpring.app.domain.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class SimpleRestController {
+//    @GetMapping("/ajax")
+//    public String ajax() {
+//        return "ajax";
+//    }
+
+    @PostMapping("/send")
+//    @ResponseBody
+    public Person test(@RequestBody Person p) {
+        System.out.println("p = " + p);
+        p.setName("ABC");
+        p.setAge(p.getAge() + 10);
+
+        return p;
+    }
+
+    @PostMapping("/send")
+//    @ResponseBody
+    public Person test2(@RequestBody Person p) {
+        System.out.println("p = " + p);
+        p.setName("ABC");
+        p.setAge(p.getAge() + 10);
+
+        return p;
+    }
+}
